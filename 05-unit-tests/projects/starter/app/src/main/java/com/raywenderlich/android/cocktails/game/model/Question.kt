0c4ce8c7aa51c31fc2212cffa9ef25bc4eb13e1d
ZakariaAFIR <zakaria.afir@leroymerlin.fr>
2022-07-16 13:17:59 +0200

    refactor: add Question class and Unit tests

---
+++ 05-unit-tests/projects/starter/app/src/main/java/com/raywenderlich/android/cocktails/game/model/Question.kt
@@ -0,0 +1,21 @@
+package com.raywenderlich.android.cocktails.game.model
+
+class Question(
+    private val correctOption: String,
+    private val incorrectOption: String
+) {
+    
+    var answeredOption: String? = null
+        private set
+    
+    private val isAnsweredCorrectly: Boolean
+        get() = correctOption == answeredOption
+    
+    fun answer(userAnswer: String): Boolean {
+        answeredOption = userAnswer
+        if (answeredOption != correctOption && answeredOption != incorrectOption) throw IllegalArgumentException(
+            "$userAnswer is different from $correctOption and $incorrectOption"
+        )
+        return isAnsweredCorrectly
+    }
+}
---
+++ 05-unit-tests/projects/starter/app/src/test/java/com/raywenderlich/android/cocktails/game/model/QuestionUnitTests.kt
@@ -0,0 +1,44 @@
+package com.raywenderlich.android.cocktails.game.model
+
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertNull
+import junit.framework.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class QuestionUnitTests {
+    
+    private lateinit var question : Question
+    @Before
+    fun setup() {
+        question = Question("CORRECT", "INCORRECT")
+    }
+    @Test
+    fun whenCreatingQuestion_shouldNotHaveAnsweredOption() {
+        assertNull(question.answeredOption)
+    }
+    
+    @Test
+    fun whenAnswering_shouldHaveAnsweredOption() {
+        question.answer("INCORRECT")
+        assertNotNull(question.answeredOption)
+    }
+    
+    @Test
+    fun whenAnswering_withCorrectOption_shouldReturnTrue() {
+        val result: Boolean = question.answer("CORRECT")
+        assertTrue("result is $result", result)
+    }
+    
+    @Test
+    fun whenAnswering_withIncorrectOption_shouldReturnFalse() {
+        val result: Boolean = question.answer("INCORRECT")
+        assertFalse("result is $result", result)
+    }
+    
+    @Test(expected = IllegalArgumentException::class)
+    fun whenAnswering_withInvalidOption_shouldThrowException() {
+        question.answer("INVALID")
+    }
+}
